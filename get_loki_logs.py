e8d8b52e4bc012bccbc3e3577f948ed0531bc851
Evan <evan.bangun.eb@gmail.com>
2025-05-22 19:57:33 +0700

    env and logs fix

--- get_loki_logs.py
+++ get_loki_logs.py
@@ -5,25 +5,23 @@ from datetime import datetime, timedelta
 import time
 import pandas as pd
 import os
+from dotenv import load_dotenv
 
 tenantDomain = set()
+load_dotenv()
 
 def get_logs(start_date, end_date):
     # Loki API endpoint
-    # url = "http://localhost:3100/loki/api/v1/query_range"
-    url = "http://10.31.67.73:3100/loki/api/v1/query_range"
-
-    pattern = re.compile(r'apiCreatorTenantDomain=([^,]+)')
+    url = os.getenv('LOKI_URL')
 
     current_date = start_date
-
     cur_end_date = end_date
 
     total_record = 0
 
-    limit = 5000
+    limit = os.getenv('LIMIT')
 
-    log_dir = 'logs'
+    log_dir = os.getenv('LOG_DIR')
     os.makedirs(log_dir, exist_ok=True)
 
     print_time = current_date
@@ -49,8 +47,7 @@ def get_logs(start_date, end_date):
 
         # Query parameters
         params = {
-            "query": '{container="splp-gw"} |~ "Metric Name: apim:response"',
-            # "query": '{container="splp-gw"}',
+            "query": os.getenv('QUERY'),
             "start": current_date,
             "end": cur_end_date,
             "limit": limit,
@@ -58,33 +55,21 @@ def get_logs(start_date, end_date):
         }
 
         new_logs_found = False
-        
-        # Print the full URL with parameters
-        # full_url = f"{url}?{'&'.join(f'{k}={v}' for k, v in params.items())}"
-        # print(f"Query URL: {full_url}\n")
 
         # Make the request
         response = requests.get(url, params=params)
-
+        
         if response.status_code == 200:
             data = response.json()
             logs_count = 0
-
-            # print("current iteration : ", current_date, cur_end_date)
-
             for stream in data['data']['result']:
                 logs_count += len(stream['values'])
                 for value in stream['values']:
                     log_content = json.loads(value[1])
-                    # print("current iteration : ", current_date, cur_end_date, logs_count, log_content["time"])
                     if current_date < log_content["time"]:
-                        # print(current_date, log_content["time"])
                         current_date = log_content["time"]
                         new_logs_found = True
                     outfile.write(json.dumps(log_content) + '\n')
-            #         match = pattern.search(log_content["log"])
-            #         if match:
-            #             tenantDomain.add(match.group(1))
             total_record += logs_count
             if logs_count < limit and cur_end_date == end_date:
                 print("last iteration : ", current_date, cur_end_date, logs_count)
@@ -99,13 +84,12 @@ def get_logs(start_date, end_date):
             current_date = cur_end_date
             cur_end_date = (pd.Timestamp(cur_end_date) + pd.Timedelta(hours=1)).isoformat().replace('+00:00', 'Z')
             print("iteration error on date range : ", current_date, cur_end_date)
-        # time.sleep(0.05)
     outfile.close()
 
     return str(total_record)
 
 if __name__ == "__main__":
-    start_date = '2025-03-20T00:00:00Z'
-    end_date = '2025-04-30T23:59:59.999999999Z'
+    start_date = os.getenv('START_DATE')
+    end_date = os.getenv('END_DATE')
 
     print("Total records = " + get_logs(start_date, end_date))
--- splp_logs_analyze.py
+++ splp_logs_analyze.py
@@ -99,40 +99,6 @@ def get_logs_allDataset():
                             "responseMediationLatency" : match["responseMediationLatency"],
                             "applicationId" : match["applicationId"]
                         })
-    # for file in folder.iterdir():
-    #     if file.is_file():
-    #         print("iterating through file : ", file.name)
-    #         with file.open("r", encoding="utf-8") as f:
-    #             for log_record in f:
-    #                 if log_record.strip():
-    #                     try:
-    #                         log_content = json.loads(log_record)
-    #                         match_apiCTD = pattern_apiCTD.search(log_content["log"]).group(1)
-    #                     except Exception as e:
-    #                         continue
-    #                     if match_apiCTD == "carbon.super":
-    #                         timestamp = log_content["time"]
-    #                         match = parse_log_line(log_content["log"])
-    #                         if match is None:
-    #                             continue
-    #                         resultDict.append({
-    #                                             "apiName" : match["apiName"], 
-    #                                             "apiCreator" : match["apiCreator"], 
-    #                                             "backendLatency" : match["backendLatency"], 
-    #                                             "requestMediationLatency" : match["requestMediationLatency"], 
-    #                                             "apiId" : match["apiId"], 
-    #                                             "applicationName" : match["applicationName"], 
-    #                                             "applicationOwner" : match["applicationOwner"],
-    #                                             "responseMediationLatency" : match["responseMediationLatency"],
-    #                                             "applicationId" : match["applicationId"]
-    #                                         })
-    # wb = openpyxl.Workbook()
-    # ws = wb.active
-    # ws.title = "All Dataset"
-    # ws.append(["apiName", "apiCreator", "backendLatency", "requestMediationLatency", "apiId", "applicationName", "applicationOwner","responseMediationLatency", "applicationId"])
-    # for data in resultDict:
-    #     ws.append([data["apiName"], data["apiCreator"], data["backendLatency"], data["requestMediationLatency"], data["apiId"], data["applicationName"], data["applicationOwner"], data["responseMediationLatency"], data["applicationId"]])
-    # wb.save("all_dataset.xlsx")
     with open('all_dataset.csv', 'w', encoding='utf-8', newline='', buffering=1024*1024) as outfile:
         writer = csv.writer(outfile)
         writer.writerow(["apiName", "apiCreator", "backendLatency", "requestMediationLatency", "apiId", "applicationName", "applicationOwner","responseMediationLatency", "applicationId"])
@@ -151,27 +117,6 @@ def get_logs_frequency_by_requester():
             append_key = ",".join([match["applicationId"], match["applicationName"], match["applicationOwner"],  match["apiName"], match["apiCreator"]])
             resultDict[append_key]["occurrence"] += 1
             resultDict[append_key]["hit_by_date"][log_date] += 1
-    # for file in folder.iterdir():
-    #     if file.is_file():
-    #         all_possible_dates.add(file.name.split(".")[0])
-    #         print("iterating through file : ", file.name)
-    #         with file.open("r", encoding="utf-8") as f:
-    #             for log_record in f:
-    #                 if log_record.strip():
-    #                     try:
-    #                         log_content = json.loads(log_record)
-    #                         match_apiCTD = pattern_apiCTD.search(log_content["log"]).group(1)
-    #                     except Exception as e:
-    #                         continue
-    #                     if match_apiCTD == "carbon.super":
-    #                         timestamp = log_content["time"]
-    #                         match = parse_log_line(log_content["log"])
-    #                         if match is None:
-    #                             continue
-    #                         if match["applicationOwner"] != match["apiCreator"] and match["proxyResponseCode"] == "200" and match["targetResponseCode"] == "200":
-    #                             append_key = ",".join([match["applicationId"], match["applicationName"], match["applicationOwner"],  match["apiName"], match["apiCreator"]])
-    #                             resultDict[append_key]["occurrence"] += 1
-    #                             resultDict[append_key]["hit_by_date"][file.name.split(".")[0]] += 1
     all_dates = normalize_dates(resultDict, all_possible_dates)
     wb = openpyxl.Workbook()
     ws = wb.active
@@ -186,14 +131,6 @@ def get_logs_frequency_by_requester():
         length = max(len(str(cell.value)) for cell in column_cells)
         ws.column_dimensions[column_cells[0].column_letter].width = length + 2
     wb.save("frequency_by_requester.xlsx")
-    # with open('frequency_by_requester.csv', 'w', encoding='utf-8', newline='', buffering=1024*1024) as outfile:
-    #     writer = csv.writer(outfile)
-    #     writer.writerow(["applicationID", "applicationName", "applicationOwner", "apiName", "Occurrence"] + all_dates)
-    #     for key, data in resultDict.items():
-    #         key_parts = key.split(",")
-    #         row = [key_parts[0], key_parts[1], key_parts[2], key_parts[3], data["occurrence"]]
-    #         row += [data["hit_by_date"][date] for date in all_dates]
-    #         writer.writerow(row)
 
 
 def get_logs_integrated_services():
@@ -207,27 +144,6 @@ def get_logs_integrated_services():
             append_key = ",".join([match["apiCreator"], match["apiName"]])
             resultDict[append_key]["occurrence"] += 1
             resultDict[append_key]["hit_by_date"][log_date] += 1
-    # for file in folder.iterdir():
-    #     if file.is_file():
-    #         all_possible_dates.add(file.name.split(".")[0])
-    #         print("iterating through file : ", file.name)
-    #         with file.open("r", encoding="utf-8") as f:
-    #             for log_record in f:
-    #                 if log_record.strip():
-    #                     try:
-    #                         log_content = json.loads(log_record)
-    #                         match_apiCTD = pattern_apiCTD.search(log_content["log"]).group(1)
-    #                     except Exception as e:
-    #                         continue
-    #                     if match_apiCTD == "carbon.super":
-    #                         timestamp = log_content["time"]
-    #                         match = parse_log_line(log_content["log"])
-    #                         if match is None:
-    #                             continue
-    #                         if match["applicationOwner"] != match["apiCreator"] and match["proxyResponseCode"] == "200" and match["targetResponseCode"] == "200":
-    #                             append_key = ",".join([match["apiCreator"], match["apiName"]])
-    #                             resultDict[append_key]["occurrence"] += 1
-    #                             resultDict[append_key]["hit_by_date"][file.name.split(".")[0]] += 1
     all_dates = normalize_dates(resultDict, all_possible_dates)
     wb = openpyxl.Workbook()
     ws = wb.active
@@ -242,14 +158,6 @@ def get_logs_integrated_services():
         length = max(len(str(cell.value)) for cell in column_cells)
         ws.column_dimensions[column_cells[0].column_letter].width = length + 2
     wb.save("integrated_services_frequency.xlsx")
-    # with open('integrated_services_frequency.csv', 'w', encoding='utf-8', newline='', buffering=1024*1024) as outfile:
-    #     writer = csv.writer(outfile)
-    #     writer.writerow(["IPPD", "apiCreator", "apiName", "Occurrence"] + all_dates)
-    #     for key, data in resultDict.items():
-    #         key_parts = key.split(",")
-    #         row = [apiCreator_to_instName.get(key_parts[0], "Tidak Terdaftar"), key_parts[0], key_parts[1], data["occurrence"]]
-    #         row += [data["hit_by_date"][date] for date in all_dates]
-    #         writer.writerow(row)
 
 
 if __name__ == "__main__":
